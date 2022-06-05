64be3b41ee464534bb79affd7fcd0c5f38b3dbe6
Lúcio Magno <devalmagno@gmail.com>
2022-06-04 21:41:13 -0300

    t4

--- t4/main.js
+++ t4/main.js
@@ -80,10 +80,95 @@ function calculateTheFatorialByGiveNumber(e) {
     const number = Number(document.querySelector('.sub_question.word_d form .input_box input').value);
     let fatorial = 1;
 
-    for (let i = number; i >= 1;i--) {
-        fatorial *= i; 
+    for (let i = number; i >= 1; i--) {
+        fatorial *= i;
     }
 
     subquestion.innerHTML = `<span><b>R:</b>O fatorial é ${fatorial}</span>`;
 }
 
+function formatDateAndShowInFull(e) {
+    e.preventDefault();
+    const subquestion = document.querySelector('.question.question_two .answer');
+    const date = document.querySelector('#date').value.split('-').reverse();
+
+    switch (date[1]) {
+        case '12':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Dezembro de ${date[2]}`;
+            break;
+        case '11':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Novembro de ${date[2]}`;
+            break;
+        case '10':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Outubro de ${date[2]}`;
+            break;
+        case '09':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Setembro de ${date[2]}`;
+            break;
+        case '08':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Agosto de ${date[2]}`;
+            break;
+        case '07':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Julho de ${date[2]}`;
+            break;
+        case '06':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Junho de ${date[2]}`;
+            break;
+        case '05':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Maio de ${date[2]}`;
+            break;
+        case '04':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Abril de ${date[2]}`;
+            break;
+        case '03':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Março de ${date[2]}`;
+            break;
+        case '02':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Fevereiro de ${date[2]}`;
+            break;
+        case '01':
+            subquestion.innerHTML = `<span><b>R: </b>${date[0]} de Janeiro de ${date[2]}`;
+            break;
+        default:
+            subquestion.innerHTML = `<span><b>Algo deu errado</b></span>`;
+            break;
+    }
+}
+
+function doTheOperationsAndSetTheTable(e) {
+    e.preventDefault();
+    const subquestion = document.querySelector('.question.question_three .answer');
+    const numbers = {
+        one: Number(document.getElementById('number_one').value),
+        two: Number(document.getElementById('number_two').value)
+    }
+
+    subquestion.innerHTML = `
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Operação</th>
+                    <th>Valor</th>
+                </tr> 
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${numbers.one} + ${numbers.two}</td>
+                    <td>${numbers.one + numbers.two}</td>
+                </tr> 
+                <tr>
+                    <td>${numbers.one} * ${numbers.two}</td>
+                    <td>${numbers.one * numbers.two}</td>
+                </tr> 
+                <tr>
+                    <td>${numbers.one} / ${numbers.two}</td>
+                    <td>${numbers.one / numbers.two}</td>
+                </tr> 
+                <tr>
+                    <td>${numbers.one} % ${numbers.two}</td>
+                    <td>${numbers.one % numbers.two}</td>
+                </tr> 
+            </tbody>
+        </table> 
+    `;
+}
